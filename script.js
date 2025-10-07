3b35a8e419f7611832821d361509f1aab9663c5a
David <96595953+xRyukx3@users.noreply.github.com>
2025-10-07 17:51:30 -0400

    Update getHumanChoice function

--- script.js
+++ script.js
@@ -13,6 +13,17 @@ function getComputerChoice() {
 function getHumanChoice() {
   const message =
     "Choose one of the following: \n 1. Rock \n 2. Paper \n 3. Scissors";
-  const humanChoice = prompt(message, 1);
+  let humanChoice = prompt(message);
+  switch (parseInt(humanChoice)) {
+    case 1:
+      humanChoice = "rock";
+      break;
+    case 2:
+      humanChoice = "paper";
+      break;
+    case 3:
+      humanChoice = "scissors";
+      break;
+  }
   return humanChoice;
 }
